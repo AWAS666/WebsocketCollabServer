ade3d26ac58b8ee42fcaf3581ad1f2d5516248e7
AWAS666 <AWAS6@protonmail.com>
2024-04-27 11:48:00 +0200

    feat: example client

---
+++ ClientExampleDotNet/Message.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ClientExampleDotNet
+{
+
+    public class Message
+    {
+        [JsonPropertyName("version")]
+        public int Version { get; set; } = 1;
+
+        /// <summary>
+        /// type for example: message
+        /// </summary>
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("from")]
+        public string From { get; set; }
+
+        [JsonPropertyName("to")]
+        public List<string> To { get; set; }
+
+        [JsonPropertyName("payload")]
+        public Payload Payload { get; set; }
+    }
+
+    public class Payload
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("content")]
+        public string Content { get; set; }
+    }
+}
---
+++ ClientExampleDotNet/Program.cs
@@ -0,0 +1,50 @@
+﻿using ClientExampleDotNet;
+using System.Text.Json;
+using WebSocketSharp;
+
+internal class Program
+{
+    private static string self = "user";
+    private static void Main(string[] args)
+    {
+        string user = "";
+        string password = "";
+
+        // this is the base url of the server + room id
+        string host = "ws://<server>/<id>";
+        using (var ws = new WebSocket(host))
+        {
+            ws.SetCredentials(user, password, true);
+            ws.OnMessage += NewMessage;
+            ws.Connect();
+
+
+            ws.Send(JsonSerializer.Serialize(new Message()
+            {
+                From = self,
+                To = new List<string> { "receiver"},
+                Type = "message",
+                Payload = new() { Name = self, Content = "Hello world!"}
+            }
+            ));
+            Console.ReadKey(true);
+        }
+        Console.ReadKey(true);
+    }
+
+    private static void NewMessage(object? sender, MessageEventArgs e)
+    {
+        var msg = JsonSerializer.Deserialize<Message>(e.Data);
+
+        // check if addressed to self
+        if (!msg.To.Contains(self))
+            return;
+
+        if (msg.Type == "message")
+        {
+            //use message
+            var name = msg.Payload.Name;
+            var content = msg.Payload.Content;
+        }
+    }
+}
